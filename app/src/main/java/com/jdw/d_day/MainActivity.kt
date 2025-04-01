6a4ba597d620bd2873ef72e54c70c946b34a18b6
Dalso13 <flskwhdhk@gmail.com>
2025-04-01 11:28:43 +0900

    feat: d-day 기능 구현

--- app/src/main/java/com/jdw/d_day/MainActivity.kt
+++ app/src/main/java/com/jdw/d_day/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.jdw.d_day
 
+import android.app.DatePickerDialog
+import java.util.concurrent.TimeUnit
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val startBtn = findViewById<Button>(R.id.startBtn)
+        val endBtn = findViewById<Button>(R.id.endBtn)
+
+        val calendarStart = Calendar.getInstance()
+        val calendarEnd = Calendar.getInstance()
+
+        startBtn.setOnClickListener {
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(
+                    view: DatePicker?,
+                    year: Int,
+                    month: Int,
+                    dayOfMonth: Int
+                ) {
+                    calendarStart.set(year,month+1,day)
+                }
+
+            }, year,month,day)
+            dlg.show()
         }
+
+        endBtn.setOnClickListener {
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(
+                    view: DatePicker?,
+                    year: Int,
+                    month: Int,
+                    dayOfMonth: Int
+                ) {
+                    calendarEnd.set(year,month+1,day)
+                    val date = TimeUnit.MICROSECONDS.toDays(calendarEnd.timeInMillis - calendarStart.timeInMillis) + 1
+
+                    findViewById<TextView>(R.id.day).setText(date.toString())
+                }
+
+            }, year,month,day)
+            dlg.show()
+        }
+
     }
 }
